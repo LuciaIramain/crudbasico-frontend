b7bfc5670d780db9b3a519d495e078e95905f7ac
Lucia Iramain <luciairamain5@gmail.com>
2021-05-07 18:01:40 -0300

    Se agrego la logica para agregar un producto a la api

--- src/components/AgregarProducto.js
+++ src/components/AgregarProducto.js
@@ -1,33 +1,131 @@
 import React, { Fragment, useState } from "react";
-import { Container, Form, Button  } from "react-bootstrap";
+import { Container, Form, Button, Alert } from "react-bootstrap";
 
 const AgregarProducto = () => {
-  const [nombreProducto, setNombreProducto] = useState('');
-  const [precioProducto, setPrecioProducto] = useState('');
-  const [categoria, setCategoria] = useState('');
+  const URL = process.env.REACT_APP_API_URL;
+  console.log(URL);
+  const [nombreProducto, setNombreProducto] = useState("");
+  const [precioProducto, setPrecioProducto] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [error, setError] = useState(false);
+
+  const cambiarCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+
+  const handleSubmit = async(e) => { //async se usa solo con el await y quiere decir que se comporta de manera asincronica y tiene que ir en la misma funcion que await
+    e.preventDefault();
+    // validar los datos
+    if (
+      nombreProducto.trim() === "" ||
+      precioProducto.trim() === "" ||
+      categoria === ""
+    ) {
+      // mostrar un cartel de error
+      setError(true);
+      return;
+    } else {
+      // enviar el producto a la api
+      setError(false);
+
+      // Crear el objeto a enviar
+      const datos = {
+        nombreProducto, // es lo mismo que nombreProducto: nombreProducto y solo lo puedo usar cuando el nombre del state y el de db.json sean iguales
+        precioProducto: precioProducto,
+        categoriaProducto: categoria
+      };
+      console.log(datos);
+      // enviar objeto a la api, operacion POST -- try{}catch(){} es una estructura de control
+      try{
+        const parametros = {
+          method: "POST", //cuando mando post, api sabe que voy a enviar un objeto y lo tengo que guardar en algun lugar
+          headers: {
+            "Content-Type": "application/json" // headers: en que formato lo mando, en este caso en json
+          },
+          body: JSON.stringify(datos)// que dato le quiero enviar
+        };
+
+        // Ejecutar la solicitud o request -- fetch es un metodo que se encarga de hacer las solicitudes
+        const respuesta = await fetch(URL, parametros); // await es para que no se ejecute la siguiente linea hasta que reciba la respuesta, se traduce(espera)
+        console.log(respuesta);
+
+      }catch(error){
+        console.log(error);
+      }
+    }
+  };
+  // las variablesde ambiente(envirornment) es un archivo dende voy a cear variables que se van a usar en cualuier lugar de mi programa
 
   return (
     <Fragment>
       <Container className="my-5">
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <h1 className="my-4 text-center">Agregar un nuevo producto</h1>
+          {error ? (
+            <Alert variant={"danger"}>Todos los campos son obligatorios</Alert>
+          ) : null}
           <Form.Group className="mb-3">
-            <Form.Label>Nombre del producto</Form.Label>
-            <Form.Control type="text" placeholder="Submarino" onChange={(e) => setNombreProducto(e.target.value)} />
+            <Form.Label>Nombre del producto *</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Submarino"
+              onChange={(e) => setNombreProducto(e.target.value)}
+            />
           </Form.Group>
           <Form.Group className="mb-3">
-            <Form.Label>Precio</Form.Label>
-            <Form.Control type="number" placeholder="$50" onChange={(e) => setPrecioProducto(e.target.value)} />
+            <Form.Label>Precio *</Form.Label>
+            <Form.Control
+              type="number"
+              placeholder="$50"
+              onChange={(e) => setPrecioProducto(e.target.value)}
+            />
           </Form.Group>
           <section className="text-center my-3">
-            <h5>Categoria</h5>
-            <Form.Check type="radio" label="Bebida caliente" name="categoria" value="bebida-caliente" onChange={(e) => setCategoria(e.target.value)} inline></Form.Check>
-            <Form.Check type="radio" label="Bebida fria" name="categoria" value="bebida-fria" onChange={(e) => setCategoria(e.target.value)} inline></Form.Check>
-            <Form.Check type="radio" label="Sandwich" name="categoria" value="sandwich" onChange={(e) => setCategoria(e.target.value)} inline></Form.Check>
-            <Form.Check type="radio" label="Dulce" name="categoria" value="dulce" onChange={(e) => setCategoria(e.target.value)} inline></Form.Check>
-            <Form.Check type="radio" label="Salado" name="categoria" value="salado" onChange={(e) => setCategoria(e.target.value)} inline></Form.Check>
+            <h5>Categoria *</h5>
+            <Form.Check
+              type="radio"
+              label="Bebida caliente"
+              name="categoria"
+              value="bebida-caliente"
+              onChange={cambiarCategoria}
+              inline
+            ></Form.Check>
+            <Form.Check
+              type="radio"
+              label="Bebida fria"
+              name="categoria"
+              value="bebida-fria"
+              onChange={cambiarCategoria}
+              inline
+            ></Form.Check>
+            <Form.Check
+              type="radio"
+              label="Sandwich"
+              name="categoria"
+              value="sandwich"
+              onChange={cambiarCategoria}
+              inline
+            ></Form.Check>
+            <Form.Check
+              type="radio"
+              label="Dulce"
+              name="categoria"
+              value="dulce"
+              onChange={cambiarCategoria}
+              inline
+            ></Form.Check>
+            <Form.Check
+              type="radio"
+              label="Salado"
+              name="categoria"
+              value="salado"
+              onChange={cambiarCategoria}
+              inline
+            ></Form.Check>
           </section>
-          <Button variant="warning" className="text-light w-100" type="submit">Agregar Producto</Button>
+          <Button variant="warning" className="text-light w-100" type="submit">
+            Agregar Producto
+          </Button>
         </Form>
       </Container>
     </Fragment>
